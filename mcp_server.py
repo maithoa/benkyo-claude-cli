7753c081b636a272a74380bd73472f0630c3feaf
maithoa <8200087+maithoa@users.noreply.github.com>
2026-05-02 11:27:47 +0300

    feat: add pre-defined prompt for mcp-server

--- mcp_server.py
+++ mcp_server.py
@@ -1,3 +1,4 @@
+from mcp.server.fastmcp.prompts import base
 from mcp.server.fastmcp import FastMCP
 from pydantic import Field
 
@@ -45,7 +46,6 @@ def edit_document(
     docs[doc_id] = docs[doc_id].replace(old_str, new_str)
     return f"Document '{doc_id}' has been updated."
 
-# TODO: Write a resource to return all doc id's
 @mcp.resource(
     "docs://documents",
     mime_type="application/json"
@@ -53,7 +53,6 @@ def edit_document(
 def list_docs() -> list[str]:
     return list(docs.keys())
 
-# TODO: Write a resource to return the contents of a particular doc
 @mcp.resource(
     "docs://documents/{doc_id}",
     mime_type="text/plain"
@@ -63,7 +62,28 @@ def fetch_doc(doc_id: str) -> str:
         raise ValueError(f"Document with id '{doc_id}' not found.")
     return docs[doc_id]
 
-# TODO: Write a prompt to rewrite a doc in markdown format
+@mcp.prompt(
+    name = "format",
+    description = "Rewrites the contents of a document in Markdown format.",
+)
+def format_document(
+    doc_id: str=Field(description="Id of the document to format")
+) -> list[base.Message]:
+    #Return a list of messages
+    prompt = f""" 
+    Your goal is to reformat a document to be written with markedown syntax. 
+    
+    The id of the document you need to reformat is 
+    <document_id>{doc_id}</document_id>.
+    
+    Add in headers, bullet points, tables, etc as necessary. 
+    Feel free to add in extra formatting . Use the 'edit_document' tool to edit the document. 
+    After the document has been reformatted, return the final contents of the document as a message with the role 'final_output' and the content being the reformatted document."""
+
+    return [base.UserMessage(content=prompt)]
+
+
+
 # TODO: Write a prompt to summarize a doc
 
 
